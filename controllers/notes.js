3f6fb4f4c5337044e4ac053523159fcbd0caa9cf
Bennett Siegel <bennett@Bennetts-MacBook-Pro.local>
2014-12-09 17:00:00 -0500

    updated

--- controllers/notes.js
+++ controllers/notes.js
@@ -15,26 +15,25 @@ exports.createNote = function(req, res, next) {
     var body = req.params.body;
     var tag = req.params.tag;
     var bucket = req.params.bucket;
-    
     if (email == undefined || body == undefined || tag == undefined || bucket == undefined || email == "" || tag == "" || bucket == "" || body == "") {
         res.send(400, {
             'STATUS_KEY': 'Please define all body, tag, and bucket.'
         });
     }
     else {
+        
         // Create note.
         var query = 'insert into notes (email, id, body, tag, bucket) values (?, now(), ?, ?, ?)';
         client.execute(query, [email, body, tag, bucket], function(err, result) {
-            
             if (err) {
                 res.send(err);
                 client.shutdown();
             }
             else {
+                
                 // Check if tag exists.
                 query = 'select id, count from tags where email =? and tag =? and bucket=? allow filtering';
                 client.execute(query, [email, tag, bucket], function(err, result) {
-                   
                     if (err) {
                         res.send(err);
                         client.shutdown();
@@ -43,10 +42,10 @@ exports.createNote = function(req, res, next) {
                         
                         // If tag does not exist.
                         if (result.rows[0] === undefined) {
+                            
                             // Create new tag record.
                             query = 'insert into tags (email, id, tag, count, bucket) values (?, now(), ?, 1, ?)';
                             client.execute(query, [email, tag, bucket], function(err, result) {
-                                
                                 if (err) {
                                     res.send(err);
                                     client.shutdown();
@@ -60,6 +59,7 @@ exports.createNote = function(req, res, next) {
                             });
                         }
                         else {
+                            
                             // If tag exists.
                             var id = result.rows[0].id;
                             var count = result.rows[0].count + 1;
@@ -72,7 +72,6 @@ exports.createNote = function(req, res, next) {
                                 query: 'insert into tags (email, id, tag, count, bucket) values (?, now(), ?, ?, ?)',
                                 params: [email, tag, count, bucket]
                             }];
-                            
                             client.batch(queries, function(err, result) {
                                 if (err) {
                                     res.send(err);
@@ -98,7 +97,6 @@ exports.getNotes = function(req, res, next) {
     var email = req.params.email;
     var arr = [];
     var page = req.params.page;
-    
     if (email == undefined || email == "") {
         res.send(400, {
             'STATUS_KEY': 'Please define email.'
@@ -106,78 +104,106 @@ exports.getNotes = function(req, res, next) {
     }
     else {
         
-        // Check for defined page.
-        if (page !== undefined) {
-
-            // Convert page to hex buffer and query on top of it.
-            page = page.toString();
-            page = new Buffer(page, 'hex');
-            query = 'select id, body, tag, dateOf(id) from notes where email =?';
-            client.eachRow(query, [email], {
-                pageState: page,
-                prepare: 1,
-                fetchSize: 100
-            }, function(n, row) {
-                arr.push({
-                    id: row.id,
-                    body: row.body,
-                    tag: row.tag
-                });
-            }, function(err, result) {
+        // Query for note before expensive multinote query.
+        query = 'select id from notes where email =? limit 1';
+        client.execute(query, [email], function(err, result) {
+            if (err) {
+                res.send(err);
+                client.shutdown();
+            }
+            else {
                 
-                var page = result.meta.pageState;
-                if (page !== undefined) {
-                    page = page.toString('hex')
-                    var notesObject = {
-                        notes: arr,
-                        page: page
-                    }
+                // If note does not exist.
+                if (result.rows[0] === undefined) {
+                    res.send(404, {
+                        'STATUS_KEY': 'Not Found'
+                    });
+                    client.shutdown();
                 }
                 else {
-                    var notesObject = {
-                        notes: arr
+                    
+                    // Check for defined page.
+                    if (page !== undefined) {
+                        
+                        // Convert page to hex buffer and query on top of it.
+                        page = page.toString();
+                        page = new Buffer(page, 'hex');
+                        query = 'select id, body, tag, dateOf(id) from notes where email =?';
+                        client.eachRow(query, [email], {
+                            pageState: page,
+                            prepare: 1,
+                            fetchSize: 100
+                        }, function(n, row) {
+                            arr.push({
+                                id: row.id,
+                                body: row.body,
+                                tag: row.tag
+                            });
+                        }, function(err, result) {
+                            console.log(result)
+                            var page = result.meta.pageState;
+                            if (page !== undefined) {
+                                page = page.toString('hex')
+                                var notesObject = {
+                                    notes: arr,
+                                    page: page
+                                }
+                            }
+                            else {
+                                var notesObject = {
+                                    notes: arr
+                                }
+                            }
+                            if (arr[0] == undefined) {
+                                res.send(404, {
+                                    'STATUS_KEY': 'Error',
+                                    'DATA': 'Not Found'
+                                })
+                            }
+                            else {
+                                res.send(200, {
+                                    'STATUS_KEY': 'Success',
+                                    'DATA': notesObject
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        
+                        // Query without page.
+                        query = 'select id, body, tag, dateOf(id) from notes where email =?';
+                        client.eachRow(query, [email], {
+                            prepare: 1,
+                            fetchSize: 100
+                        }, function(n, row) {
+                            arr.push({
+                                id: row.id,
+                                body: row.body,
+                                tag: row.tag
+                            });
+                        }, function(err, result) {
+                            var page = result.meta.pageState;
+                            if (page !== undefined) {
+                                page = page.toString('hex')
+                                var notesObject = {
+                                    notes: arr,
+                                    page: page
+                                }
+                            }
+                            else {
+                                var notesObject = {
+                                    notes: arr
+                                }
+                            }
+                            res.send(200, {
+                                'STATUS_KEY': 'Success',
+                                'DATA': notesObject
+                            });
+                        });
                     }
                 }
-                res.send(200, {
-                    'STATUS_KEY': 'Success',
-                    'DATA': notesObject
-                });
-            });
-        }
-        else {
-
-            // Query without page.
-            query = 'select id, body, tag, dateOf(id) from notes where email =?';
-            client.eachRow(query, [email], {
-                prepare: 1,
-                fetchSize: 100
-            }, function(n, row) {
-                arr.push({
-                    id: row.id,
-                    body: row.body,
-                    tag: row.tag
-                });
-            }, function(err, result) {
-                var page = result.meta.pageState;
-                
-                if (page !== undefined) {
-                    page = page.toString('hex')
-                    var notesObject = {
-                        notes: arr,
-                        page: page
-                    }
-                }
-                else {
-                    var notesObject = {
-                        notes: arr
-                    }
-                }
-                res.send(200, {
-                    'STATUS_KEY': 'Success',
-                    'DATA': notesObject
-                });
-            });
-        }
+            }
+        })
     }
 }
 exports.getNote = function(req, res, next) {
@@ -185,7 +211,6 @@ exports.getNote = function(req, res, next) {
     // Set note variables.
     var email = req.params.email;
     var id = req.params.id;
-    
     if (email == undefined || id == undefined || email == "" || id == "") {
         res.send(400, {
             'STATUS_KEY': 'Please define id.'
@@ -201,7 +226,7 @@ exports.getNote = function(req, res, next) {
                 client.shutdown();
             }
             else {
-               
+                
                 // If note does not exist.
                 if (result.rows[0] === undefined) {
                     res.send(404, {
@@ -222,14 +247,13 @@ exports.deleteNote = function(req, res, next) {
     // Set note variables.    
     var email = req.params.email;
     var id = req.params.id;
-    
     if (email == undefined || id == undefined || email == "" || id == "") {
         res.send(400, {
             'STATUS_KEY': 'Please define id.'
         });
     }
     else {
-
+        
         // Delete the note.
         query = 'delete from tags where email =? and id =?';
         client.execute(query, [email, id], function(err, result) {
@@ -245,5 +269,3 @@ exports.deleteNote = function(req, res, next) {
             }
         })
     }
-}
-
